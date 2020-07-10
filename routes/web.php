4293bd72f9afdd1cde29374d890e1af539cb8212
fumiya-sasaki <09.fumi.03@gmail.com>
2020-07-10 04:45:57 +0000

    課題09の修正

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('xxx' , 'AAAController@bbb');
+
+Route::get('admin/profile/create' , 'Admin\ProfileController@add');
+
+Route::get('admin/profile/edit' , 'Admin\ProfileController@edit');
