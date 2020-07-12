bdfdcaa5d6d736e88a699b32e5cfde72972b7899
fumiya-sasaki <09.fumi.03@gmail.com>
2020-07-12 06:03:47 +0000

    課題14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,8 +12,15 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $news = new Profile;
+        $form = $request->all();
+        unset($form['_token']);
+        $news->fill($form);
+        $news->save();
+        
         return redirect('admin/profile/create');
     }
 
